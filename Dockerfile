2932cb4f84294e668068dfe50dcd5b19c990be66
Bozhidar Hristov <warxcell@gmail.com>
2020-12-11 19:03:00 +0200

    initial

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:10-slim
+
+RUN apt-get update \
+    && apt-get install -y wget gnupg \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-unstable
+
+COPY ./pdf_generator/package*.json ./
+RUN yarn install
+
+COPY ./pdf_generator .
+
+ENTRYPOINT node index.js
+
+EXPOSE 3000
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+function rawBody(req, res, next) {
+    req.setEncoding('utf8');
+    req.rawBody = '';
+    req.on('data', function (chunk) {
+        req.rawBody += chunk;
+    });
+    req.on('end', function () {
+        next();
+    });
+}
+
+app.use(rawBody);
+
+app.post('/', async (req, res) => {
+    try {
+        console.log("Converting...");
+
+        const pdf = await generatePDF(req.rawBody);
+
+        res.set('Content-Type', 'application/pdf');
+        res.set('Content-Length', pdf.length);
+        res.send(pdf);
+    } catch (e) {
+        console.error(e);
+        return res.sendStatus(500);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
+
+async function generatePDF(html) {
+    try {
+        console.log("Launching browser...");
+
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox'],
+        });
+
+
+        console.log("Opening new page...");
+
+        const page = await browser.newPage();
+
+        page.on('requestfailed', request => {
+            console.log(`url: ${request.url()}, errText: ${request.failure().errorText}, method: ${request.method()}`)
+        });
+        // Catch console log errors
+        page.on("pageerror", err => {
+            console.log(`Page error: ${err.toString()}`);
+        });
+
+        console.log("Setting content...");
+        await page.setContent(html, {'waitUntil': 'networkidle0'});
+
+        console.log("Print to PDF...");
+
+        return await page.pdf({format: 'A4', printBackground: true});
+    } catch (e) {
+        console.error(e);
+    } finally {
+        console.log("Close browser...");
+        await browser.close();
+    }
+}
